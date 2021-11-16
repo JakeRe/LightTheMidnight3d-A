b75def915883051cb88968ba06f8d5a51391a5d1
JakeRe <jake.reilly.20@gmail.com>
2021-11-16 16:31:08 -0600

    Started adding some stuff for dialogue.

---
+++ Assets/TutorialInteraction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialInteraction : MonoBehaviour
+{
+    void TimeStop()
+    {
+        Time.timeScale = 0;
+    }
+}
