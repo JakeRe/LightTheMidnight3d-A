33ae72d275e088547a795986662e207f27e8bae6
JakeRe <jake.reilly.20@gmail.com>
2021-12-10 09:55:24 -0600

    Added multiple components for tutorial.

--- Assets/Script/Tutorial.cs
+++ Assets/Script/Tutorial.cs
@@ -5,65 +5,78 @@ using UnityEngine.Playables;
 
 public class Tutorial : MonoBehaviour
 {
-    [SerializeField] private AudioSource speaker;
+    [Header("Arrays for Tutorial Timelines and Dialogue")]
     [SerializeField] private PlayableAsset[] tutorials;
     [SerializeField] private GameObject[] dialogueBoxes;
+    [Header("Variables that track what dialogue has pased")] 
     [SerializeField] private int dialoguePassed;
-    [SerializeField] private int maxDialogue;
+    [SerializeField] private int currentDialogue;
+    [Header("Dependant Components")]
     [SerializeField] private PlayerController player;
     [SerializeField] private PlayableDirector playDirect;
+    [SerializeField] private GameObject firstDoor;
 
     private void Start()
     {
         player = FindObjectOfType<PlayerController>();
-        dialoguePassed = 1;
+        dialoguePassed = 0;
+        currentDialogue = 1;
         dialogueBoxes[1].SetActive(false);
     }
     public void IncrementDialoguePassed()
     {
-        dialoguePassed++;
+       dialoguePassed++;
     }
 
 
     public void Update()
     {
-        CheckDialogue(playDirect);
+        CheckDialogue();
     }
 
-    private void OnEnable()
+   
+    public void CheckDialogue()
     {
-        playDirect.stopped += CheckDialogue;
-    }
-
-    private void OnDisable()
-    {
-        playDirect.stopped += CheckDialogue;
-    }
-
-    public void CheckDialogue(PlayableDirector director)
-    {
-        switch (dialoguePassed)
+        switch (currentDialogue)
         {
             case 1:
                 dialogueBoxes[0].SetActive(true);
                 playDirect.playableAsset = tutorials[0];
                 playDirect.Play();
+                StartCoroutine(WaitForDialogueToFinish());
+                currentDialogue++;
                 break;
             case 2:
-                Debug.Log("Second Dialogue Ready!");
                 if (player.health < player.maxHealth)
                 {
                     dialogueBoxes[1].SetActive(true);
                     playDirect.playableAsset = tutorials[1];
                     playDirect.Play();
+                    StartCoroutine(WaitForDialogueToFinish());
+                    currentDialogue++;
+                    dialogueBoxes[1].SetActive(false);
                 }
                 break;
             case 3:
-                Debug.Log("Third Dialogue Ready!");
                 if (player.playerPoints > 0)
                 {
+                    dialogueBoxes[2].SetActive(true);
                     playDirect.playableAsset = tutorials[2];
                     playDirect.Play();
+                    StartCoroutine(WaitForDialogueToFinish());
+                    currentDialogue++;
+                    dialogueBoxes[2].SetActive(false);
+                }
+                break;
+            case 4:
+                if(firstDoor = null)
+                {
+                    dialogueBoxes[3].SetActive(true);
+                    playDirect.playableAsset = tutorials[3];
+                    playDirect.Play();
+                    StartCoroutine(WaitForDialogueToFinish());
+                    currentDialogue++;
+                    dialogueBoxes[3].SetActive(false);
                 }
                 break;
             default:
@@ -72,5 +85,9 @@ public class Tutorial : MonoBehaviour
         }
     }
 
+    IEnumerator WaitForDialogueToFinish()
+    {
+        yield return new WaitUntil(() => dialoguePassed >= currentDialogue);
+    }
     
 }
